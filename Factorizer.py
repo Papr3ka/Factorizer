c6b9fd66b27c007e1600e044ae35af2c197d385a
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2019-12-11 14:00:34 -0500

    Update Factorizer.py

--- Factorizer.py
+++ Factorizer.py
@@ -8,19 +8,45 @@ def clear():
         os.system('cls')
     else:
         os.system('clear')
+def wait():
+    if waitsetc == 2:
+        wait = str(input("Press enter to continue..."))
+        clear()
+    if waitsetc == 1:
+        clear()
+        wait = str(input("Press enter to continue..."))
+    if waitsetc == 0:
+        wait = str(input("Press enter to exit..."))
 def inttest(x):
     decval = int(x) - x
     if decval == 0:
         return True
     else:
         return False
+waitsetc = 1
 clear()
 fact = int(input("Factorize:"))
+wait()
 clear()
-factors = [fact]
-for x in range(1,fact,1):
+print("Factorizing...")
+factors = [1]
+tstart = time.perf_counter()
+for x in range(1,int(fact / 2) + 1,1):
     ans = fact / x
     if inttest(ans):
-        print(x)
-        factors.append(ans)
-        
+        factors.append(int(ans))
+tend = time.perf_counter()
+factors.sort()
+clear()
+print("Done\n")
+if len(factors) > 2:
+    print(fact,"Has",len(factors),"Factors")
+else:
+    print(fact,"Is a prime number")
+print("Showing Factors for",fact)
+print("\n")
+for x in factors:
+    print(x)
+print("\nTime",int((tend - tstart) * 10000) / 10000,"Seconds")
+waitsetc = 0
+wait()        
